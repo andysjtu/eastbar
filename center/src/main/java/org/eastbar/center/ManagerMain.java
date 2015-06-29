fad2185f9ff0b100cc2461dd8933269228b30585
c.lins <c.lins@qq.com>
2015-06-29 14:20:32 +0800

    .

--- center/src/main/java/org/eastbar/center/ManagerMain.java
+++ center/src/main/java/org/eastbar/center/ManagerMain.java
@@ -1,5 +1,6 @@
 package org.eastbar.center;
 
+import org.eastbar.center.customerLog.service.CustomerService;
 import org.eastbar.center.net.CenterListener;
 import org.eastbar.center.statusMachine.IEventPipe;
 import org.eastbar.center.statusMachine.core.EventPipe;
